a195232ad952f26532ac25ba9509c07fee90feb6
Chardies <chardies@users.noreply.github.com>
2019-04-08 22:55:43 +0800

    Updates

---
+++ precache-manifest.521c41a46cfd866a61a9545cca64d924.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "492258ee7fe6ce9345db",
+    "url": "/isoline/static/js/runtime~main.17139328.js"
+  },
+  {
+    "revision": "f588bc79f2e04e97d48b",
+    "url": "/isoline/static/js/main.dfac3566.chunk.js"
+  },
+  {
+    "revision": "ab7f1fde94af86772fd5",
+    "url": "/isoline/static/js/2.4eca978d.chunk.js"
+  },
+  {
+    "revision": "f588bc79f2e04e97d48b",
+    "url": "/isoline/static/css/main.6ea4fe2a.chunk.css"
+  },
+  {
+    "revision": "d80ed9a716dda07cd9ef78bd14865d3e",
+    "url": "/isoline/index.html"
+  }
+];
